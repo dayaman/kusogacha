329cdee42b07eb70801625c151f3b6571e549d50
dayaman <s16t279@stu.kagawa-u.ac.jp>
2018-05-24 15:42:10 +0900

    モデルを定義

--- gacha/__init__.py
+++ gacha/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('getmona.config')
+app.config.from_object('gacha.config')
 db = SQLAlchemy(app)
 
 import gacha.views
